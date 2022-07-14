3d45ebba3b7c755de18bb63c1c3206784357d39f
Bella <67583659+honeylemonicetea@users.noreply.github.com>
2022-07-15 00:45:41 +0300

    added

--- songpop/settings.py
+++ songpop/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import os 
+import os
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -132,3 +133,4 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+django_heroku.settings(locals())
