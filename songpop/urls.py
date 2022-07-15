8e579f7d5400538cceea84f4765bcfc529050912
Bella <67583659+honeylemonicetea@users.noreply.github.com>
2022-07-15 18:21:17 +0300

    fix routes

--- songpop/urls.py
+++ songpop/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 # from songs.views import view_all
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", TemplateView.as_view(template_name='index.html'), name=""),
+    re_path(r"*", TemplateView.as_view(template_name='index.html'), name=""),
     # path('all/', view_all),
     path('api/',include('songs.urls'))
 ]
